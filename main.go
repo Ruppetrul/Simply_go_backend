c5e5123a6b55a2dd9d6c9a21ba7f7fdf9c39fbb2
serg <nautbek@gmail.com>
2024-05-11 16:04:01 +0300

    Add products endpoint.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type product struct {
+	Id    int     `json:"id"`
 	Name  string  `json:"name"`
 	Price float32 `json:"price"`
 }
@@ -21,31 +22,9 @@ func main() {
 	dbConnection()
 
 	http.HandleFunc("/product", productHandler)
+	http.HandleFunc("/products", productsHandler)
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
-		return
-	}
-}
-
-func productHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(w, "Method not allowed. Only GET supported.", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var testProduct = new(product)
-	testProduct.Name = "Test product"
-	testProduct.Price = 777.77
-
-	jsonData, err := json.Marshal(testProduct)
-	if err != nil {
-		http.Error(w, "Json convert error", http.StatusInternalServerError)
-		return
-	}
-
-	_, err = fmt.Fprintf(w, string(jsonData))
-	if err != nil {
-		fmt.Println("Response error:", err)
 		return
 	}
 }
@@ -71,4 +50,63 @@ func dbConnection() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected to database")
+}
+
+func productHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method not allowed. Only GET supported.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var testProduct = new(product)
+	testProduct.Name = "Test product"
+	testProduct.Price = 777.77
+
+	jsonData, err := json.Marshal(testProduct)
+	if err != nil {
+		http.Error(w, "Json convert error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, string(jsonData))
+	if err != nil {
+		fmt.Println("Response error:", err)
+		return
+	}
+}
+
+func productsHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "Method not allowed. Only GET supported.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var products []product
+
+	rows, err := db.Query("SELECT * FROM products")
+	if err != nil {
+		http.Error(w, "Query error", http.StatusInternalServerError)
+		return
+	}
+
+	for rows.Next() {
+		var p product
+		if err = rows.Scan(&p.Id, &p.Name, &p.Price); err != nil {
+			fmt.Println("Response error:", err)
+			http.Error(w, "Scan error", http.StatusInternalServerError)
+			return
+		}
+		products = append(products, p)
+	}
+
+	jsonData, err := json.Marshal(products)
+	if err != nil {
+		http.Error(w, "Json convert error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, string(jsonData))
+	if err != nil {
+		fmt.Println("Response error:", err)
+	}
 }
